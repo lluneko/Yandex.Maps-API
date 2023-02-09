2892f447112e96f9b17e7035f46faa7bd4eee582
lluneko <117767742+lluneko@users.noreply.github.com>
2023-02-09 17:15:57 +0300

    main
    Signed-off-by: lluneko <117767742+lluneko@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print('olya')
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+class MyWidget(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setObjectName("MainWindow")
+        self.resize(443, 437)
+        self.setStyleSheet("background-color: rgb(186, 248, 105);")
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(30, 0, 391, 381))
+        self.label.setObjectName("label")
+        self.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(self)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 443, 26))
+        self.menubar.setObjectName("menubar")
+        self.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(self)
+        self.statusbar.setObjectName("statusbar")
+        self.setStatusBar(self.statusbar)
+
+        self.retranslateUi(self)
+        QtCore.QMetaObject.connectSlotsByName(self)
+        self.label.setText("TextLabel")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
