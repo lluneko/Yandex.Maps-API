d84bb4d6aac3b87caa3c5678aacd34aab4aa1671
lluneko <117767742+lluneko@users.noreply.github.com>
2023-02-09 17:17:01 +0300

    main
    Signed-off-by: lluneko <117767742+lluneko@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
 class MyWidget(QMainWindow):
-
     def __init__(self):
         super().__init__()
         self.setObjectName("MainWindow")
@@ -23,7 +22,6 @@ class MyWidget(QMainWindow):
         self.statusbar.setObjectName("statusbar")
         self.setStatusBar(self.statusbar)
 
-        self.retranslateUi(self)
         QtCore.QMetaObject.connectSlotsByName(self)
         self.label.setText("TextLabel")
 
