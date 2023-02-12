a8ea40e281ae2affbde79fd1fa8cb65659fb84c0
lluneko <117767742+lluneko@users.noreply.github.com>
2023-02-12 20:34:56 +0300

    main
    Signed-off-by: lluneko <117767742+lluneko@users.noreply.github.com>

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ class MyWidget(QMainWindow):
             print("неверные данные")
             coord_x, coord_y = float(input()), float(input())
         print('Введите масштаб')
-        z = float(input())
+        z = int(input())
         self.setObjectName("MainWindow")
-        self.resize(2500, 1000)
+        self.resize(550, 500)
         self.setStyleSheet("background-color: rgb(186, 248, 105);")
         self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(30, 0, 391, 381))
+        self.label.setGeometry(QtCore.QRect(20, 10, 480, 490))
         self.label.setObjectName("label")
         self.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(self)
@@ -34,7 +34,7 @@ class MyWidget(QMainWindow):
         QtCore.QMetaObject.connectSlotsByName(self)
         self.label.setText("TextLabel")
 
-        m = f"http://static-maps.yandex.ru/1.x/?ll={coord_x, coord_y}&z={z}&l={type}"
+        m = f"http://static-maps.yandex.ru/1.x/?ll={coord_x},{coord_y}&z={z}&l=map"
         response = requests.get(m)
         if not response:
             print("Ошибка выполнения запроса:")
@@ -46,7 +46,7 @@ class MyWidget(QMainWindow):
             file.write(response.content)
 
         img = QtGui.QPixmap(im)
-        self.label_5.setPixmap(img)
+        self.label.setPixmap(img)
 
 
 if __name__ == '__main__':
