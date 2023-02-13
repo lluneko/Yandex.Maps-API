cc590767937eddf74d06188e946769dc3dc51b6b
lluneko <117767742+lluneko@users.noreply.github.com>
2023-02-13 17:47:29 +0300

    main
    Signed-off-by: lluneko <117767742+lluneko@users.noreply.github.com>

--- main.py
+++ main.py
@@ -47,35 +47,37 @@ class MyWidget(QMainWindow):
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageUp:
-            self.z += 1
-            m = f"http://static-maps.yandex.ru/1.x/?ll={self.coord_x},{self.coord_y}&z={self.z}&l=map"
-            response = requests.get(m)
-            if not response:
-                print("Ошибка выполнения запроса:")
-                print(m)
-                print("Http статус:", response.status_code, "(", response.reason, ")")
-                sys.exit(1)
-            im = "m.png"
-            with open(im, "wb") as file:
-                file.write(response.content)
+            if self.z < 17:
+                self.z += 1
+                m = f"http://static-maps.yandex.ru/1.x/?ll={self.coord_x},{self.coord_y}&z={self.z}&l=map"
+                response = requests.get(m)
+                if not response:
+                    print("Ошибка выполнения запроса:")
+                    print(m)
+                    print("Http статус:", response.status_code, "(", response.reason, ")")
+                    sys.exit(1)
+                im = "m.png"
+                with open(im, "wb") as file:
+                    file.write(response.content)
 
-            img = QtGui.QPixmap(im)
-            self.label.setPixmap(img)
+                img = QtGui.QPixmap(im)
+                self.label.setPixmap(img)
         if event.key() == Qt.Key_PageDown:
-            self.z -= 1
-            m = f"http://static-maps.yandex.ru/1.x/?ll={self.coord_x},{self.coord_y}&z={self.z}&l=map"
-            response = requests.get(m)
-            if not response:
-                print("Ошибка выполнения запроса:")
-                print(m)
-                print("Http статус:", response.status_code, "(", response.reason, ")")
-                sys.exit(1)
-            im = "m.png"
-            with open(im, "wb") as file:
-                file.write(response.content)
+            if self.z > 1:
+                self.z -= 1
+                m = f"http://static-maps.yandex.ru/1.x/?ll={self.coord_x},{self.coord_y}&z={self.z}&l=map"
+                response = requests.get(m)
+                if not response:
+                    print("Ошибка выполнения запроса:")
+                    print(m)
+                    print("Http статус:", response.status_code, "(", response.reason, ")")
+                    sys.exit(1)
+                im = "m.png"
+                with open(im, "wb") as file:
+                    file.write(response.content)
 
-            img = QtGui.QPixmap(im)
-            self.label.setPixmap(img)
+                img = QtGui.QPixmap(im)
+                self.label.setPixmap(img)
         event.accept()
 
 
