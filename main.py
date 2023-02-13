8eccf4c91deac434f2db9dada2de276e51a3396d
lluneko <117767742+lluneko@users.noreply.github.com>
2023-02-13 17:23:40 +0300

    main
    Signed-off-by: lluneko <117767742+lluneko@users.noreply.github.com>

--- main.py
+++ main.py
@@ -63,6 +63,25 @@ class MyWidget(QMainWindow):
                 print("Http статус:", response.status_code, "(", response.reason, ")")
                 sys.exit(1)
             im = "m.png"
+            with open(im, 'wb'):
+                pass
+            with open(im, "wb") as file:
+                file.write(response.content)
+
+            img = QtGui.QPixmap(im)
+            self.label.setPixmap(img)
+        if event.key() == Qt.Key_PageDown:
+            self.z -= 1
+            m = f"http://static-maps.yandex.ru/1.x/?ll={self.coord_x},{self.coord_y}&self.z={self.z}&l=map"
+            response = requests.get(m)
+            if not response:
+                print("Ошибка выполнения запроса:")
+                print(m)
+                print("Http статус:", response.status_code, "(", response.reason, ")")
+                sys.exit(1)
+            im = "m.png"
+            with open(im, 'wb'):
+                pass
             with open(im, "wb") as file:
                 file.write(response.content)
 
